9e70e6d48381e6cc73489a3f88456d77b8e13015
adnan-mujagic <adnanmujagic@outlook.com>
2022-11-09 22:42:55 +0100

    Refactor, removed comments

--- quotesRepository.js
+++ quotesRepository.js
@@ -8,12 +8,10 @@ module.exports = {
         
         var out = new Array(limit);
 
-        // Make a copy of the qoutes array so that we can modify it while not touching the original array.
         var availableQuotes = quotes.slice();
 
         for (var i = 0; i < limit; i++) {
-            // Take away one element from available quotes and assign it to out at position i.
-            out[i] = availableQuotes.splice([Math.floor(Math.random() * availableQuotes.length)], 1)[0];
+            out[i] = availableQuotes.splice(Math.floor(Math.random() * availableQuotes.length), 1)[0];
         }
         
         return out;
