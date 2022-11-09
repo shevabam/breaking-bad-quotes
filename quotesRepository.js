32c90d209812b7df6d1f93f51a6f6a3cafe2dcc0
adnan-mujagic <adnanmujagic@outlook.com>
2022-11-09 22:22:03 +0100

    Improved the random function

--- quotesRepository.js
+++ quotesRepository.js
@@ -5,15 +5,15 @@ var quotes = require('./quotes');
 module.exports = {
     getRandom: function getRandom(numberOfQuotes) {
         var limit = numberOfQuotes > quotes.length ? quotes.length : numberOfQuotes;
-
+        
         var out = new Array(limit);
-        var quote;
+
+        // Make a copy of the qoutes array so that we can modify it while not touching the original array.
+        var availableQuotes = quotes.slice();
 
         for (var i = 0; i < limit; i++) {
-            do {
-                quote = quotes[Math.floor(Math.random() * quotes.length)];
-            } while (out.indexOf(quote) > -1);
-            out[i] = quote;
+            // Take away one element from available quotes and assign it to out at position i.
+            out[i] = availableQuotes.splice([Math.floor(Math.random() * availableQuotes.length)], 1)[0];
         }
         
         return out;
