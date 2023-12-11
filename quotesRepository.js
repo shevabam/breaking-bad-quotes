ccec3c1eb795f9b75bee298ab712b54bbe85d095
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-12-11 15:21:01 +0200

    feat: use json file for quotes list & format all files

--- quotesRepository.js
+++ quotesRepository.js
@@ -1,19 +1,19 @@
-'use strict';
-
-var quotes = require('./quotes');
+const quotes = require("./quotes.json");
 
 module.exports = {
-    getRandom: function getRandom(numberOfQuotes) {
-        var limit = numberOfQuotes > quotes.length ? quotes.length : numberOfQuotes;
-        
-        var out = new Array(limit);
+  getRandom: function getRandom(numberOfQuotes) {
+    const limit =
+      numberOfQuotes > quotes.length ? quotes.length : numberOfQuotes;
+    const out = new Array(limit);
+    const availableQuotes = quotes.slice();
 
-        var availableQuotes = quotes.slice();
-
-        for (var i = 0; i < limit; i++) {
-            out[i] = availableQuotes.splice(Math.floor(Math.random() * availableQuotes.length), 1)[0];
-        }
-        
-        return out;
+    for (const i = 0; i < limit; i++) {
+      out[i] = availableQuotes.splice(
+        Math.floor(Math.random() * availableQuotes.length),
+        1
+      )[0];
     }
+
+    return out;
+  },
 };
